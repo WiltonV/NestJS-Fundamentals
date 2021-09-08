88efd327ce01125d611184f4b352e73cc70c093c
Wilton Vieira <wlv.wilton@gmail.com>
2021-09-08 17:31:54 -0300

    chapter 2

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Remove os excedentes da request
+      transform: true, // Executa transformação automática dos tipos de dados
+      forbidNonWhitelisted: true, // Não permite que sejam enviados itens a mais na request
+    }),
+  );
+  await app.listen(3333);
 }
 
 bootstrap();
